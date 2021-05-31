0e72342fa81ab8f6bbd9915dd3872cf02612a926
riskids <dwisriski@yahoo.com>
2021-05-31 21:59:36 +0700

    add responsive webcam

--- src/Component/Camera.js
+++ src/Component/Camera.js
@@ -1,3 +1,6 @@
+import Webcam from "react-webcam";
+import React from 'react';
+
 function Camera() {
   return (
     <div>
@@ -9,6 +12,19 @@ function Camera() {
     <div className="tab-content" id="nav-tabContent">
       <div className="tab-pane fade show active" id="nav-camera" role="tabpanel">
         {/* Disini Codingan Camera  */}
+        <Webcam
+          muted={true} 
+          style={{
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: "100%",
+            height: "inherit",
+          }}
+        />
       </div>
     </div>
   </div>
