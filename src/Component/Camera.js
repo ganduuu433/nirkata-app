e41859614f08bfb747044e564832e09b91b1867b
ardyh <13517062@std.stei.itb.ac.id>
2021-06-02 09:48:21 +0700

    not finished: feeding webcam capture to model

--- src/Component/Camera.js
+++ src/Component/Camera.js
@@ -1,7 +1,32 @@
 import Webcam from "react-webcam";
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {predict, initModel} from '../MLEngine/inference.js'
 
 function Camera() {
+  const [pred, setPred] = useState("");
+
+  const showPrediction = async (webcam, mobilenet, model) => {
+    let pred = await predict(webcam, mobilenet, model);
+    setPred(pred);
+  };
+
+  const initInference = async () => {
+    const {webcam, mobilenet, model} = initModel();
+    await webcam.setup();
+    // setInterval(
+    //   () => showPrediction(),
+    //   200
+    // );
+    showPrediction(webcam, mobilenet, model);
+  };
+
+  useEffect(
+    () => {
+      initInference();
+    },
+    []
+  );
+
   return (
     <div>
     <nav>
@@ -13,6 +38,7 @@ function Camera() {
       <div className="tab-pane fade show active" id="nav-camera" role="tabpanel">
         {/* Disini Codingan Camera  */}
         <Webcam
+          id="webcam"
           audio={false}
           muted={true} 
           style={{
@@ -30,16 +56,14 @@ function Camera() {
         <div className="signDetection">
           <ul>
             <li id="sign"><h5>sign</h5></li>
-            <li id="signResult"><h5><strong>A</strong></h5></li>
+            <li id="signResult"><h5><strong>{pred}</strong></h5></li>
             <li>   
             </li>
             <button type="button" className="signButton">
                 <img src="img/pencil-icon.png"></img>
               </button>
-          </ul>
-          
+          </ul>  
         </div>
-        
       </div>
     </div>
   </div>
---
+++ src/MLEngine/inference.js
@@ -0,0 +1,52 @@
+import Webcam from './webcam.js'
+import * as tf from '@tensorflow/tfjs'
+
+const predictionMap = {
+    0: 'A',
+    1: 'B',
+    2: 'C',
+    3: 'D',
+    4: 'E',
+    5: 'O',
+    6: 'U',
+    7: 'V',
+    8: 'W'
+}
+
+async function loadMobilenet() {
+    const mobilenet = await tf.loadLayersModel('https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_1.0_224/model.json');
+    const layer = mobilenet.getLayer('conv_pw_13_relu');
+    return tf.model({inputs: mobilenet.inputs, outputs: layer.output});
+}
+
+// predict function for react app
+async function predict(webcam, mobilenet, model) {
+    const predictedClass = tf.tidy(() => {
+        const img = webcam.capture();
+        const activation = mobilenet.predict(img);
+        const predictions = model.predict(activation);
+        return predictions.as1D().argMax();
+    });
+    let preds = await predictedClass.data();
+    const classId = preds[0];
+        
+    predictedClass.dispose();
+    await tf.nextFrame();
+
+    return predictionMap[classId]
+}
+
+async function initModel() {
+    const fps = 5;
+    const fpsInterval = 1000 / fps; 
+    
+    const webcam = new Webcam(document.getElementById('webcam'));
+    
+    const mobilenet = await loadMobilenet();
+    const modelURL = './MLEngine/mobilenet-ae-20/my_model.json'
+    const model = await tf.loadLayersModel(modelURL);
+
+    return {webcam, mobilenet, model}
+}
+
+export {predict, initModel};
